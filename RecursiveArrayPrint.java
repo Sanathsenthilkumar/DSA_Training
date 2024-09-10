b9cb474231e560072ddc10c3e7f92c06f0d493c4
Sanathsenthilkumar <166885449+Sanathsenthilkumar@users.noreply.github.com>
2024-09-10 09:04:46 +0530

    Add files via upload

---
+++ RecursiveArrayPrint.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+//Program to print array using recursion
+public class RecursiveArrayPrint {
+    public static void arrayPrint(int[] a, int len,int i){
+        if(i>len-1)
+            return ;
+        System.out.print(a[i]+" ");
+        arrayPrint(a,len,i+1);
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int size = scn.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++)
+            arr[i] = scn.nextInt();
+        arrayPrint(arr, size,0);
+        scn.close();
+    }
+}
